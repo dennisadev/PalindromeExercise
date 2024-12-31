1a9d49a18bfbfc950ce09c503c521cc85e7fa144
Dennis <dennisadev@gmail.com>
2024-12-31 15:36:53 -0500

    Inital commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            char[] chars = word.ToCharArray();
+            Array.Reverse(chars);
+            string reversed  = new string(chars);
+
+            if (word == reversed)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,20 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("fail", false)]
+        [InlineData("kayak", true)]
+        public void Test1(string word, bool expected)
         {
+            //Arange
+            var wordSmithTest = new WordSmith();
+            //Act
+            var actual = wordSmithTest.IsAPalindrome(word);
 
+            //Assert
+            Assert.Equal(expected, actual);
+            
         }
     }
 }
